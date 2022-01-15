88eae276ea8d040b875468bb974a5df69b39ced2
Matheus Genteluci <mgenteluci97@gmail.com>
2022-01-15 00:34:52 -0300

    build
    - create codestar-connection template
    - create pipeline template initial version

---
+++ infrastructure/commons/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function die {
+    declare MSG="$@"
+    echo -e "$0: Error: $MSG">&2
+    exit 1
+}
+
+aws cloudformation deploy \
+    --template-file template.yml \
+    --stack-name CommonResources || die 'CloudFormation Deploy Failed'
---
+++ infrastructure/pipeline/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function die {
+    declare MSG="$@"
+    echo -e "$0: Error: $MSG">&2
+    exit 1
+}
+
+aws cloudformation deploy \
+    --template-file template.yml \
+    --stack-name AsyncMailSenderPipeline \
+    --capabilities CAPABILITY_NAMED_IAM || die 'CloudFormation Deploy Failed'
